49daf55583e374874b2db31ad3cad33fef23140d
Student 1 <student1@Nicks-Codes-MacBook-Air.local>
2024-03-01 12:14:10 -0500

    counter not finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import * as XLSX from "xlsx";
-import "./App.css"
+import "./App.css";
 function App() {
   // onchange states
   const [excelFile, setExcelFile] = useState(null);
   const [typeError, setTypeError] = useState(null);
   // submit state
   const [excelData, setExcelData] = useState(null);
+  const [assignmentCounter, setAssignmentCounter] = useState(null);
   const [count, setCount] = useState(0);
   const fixedData = [];
-  const tempArr = []
+
   // onchange event
   const handleFile = (e) => {
     let fileTypes = [
@@ -17,8 +18,11 @@ function App() {
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
       "text/csv",
     ];
+
     let selectedFile = e.target.files;
-    for(let i =0;i<selectedFile.length;i++){
+
+    const tempArr = [];
+    for (let i = 0; i < selectedFile.length; i++) {
       if (selectedFile[i]) {
         if (selectedFile[i] && fileTypes.includes(selectedFile[i].type)) {
           setTypeError(null);
@@ -35,29 +39,30 @@ function App() {
         console.log("Please select your file");
       }
     }
-    setExcelFile(tempArr)
+    setExcelFile(tempArr);
   };
   // submit event
   const handleFileSubmit = (e) => {
     e.preventDefault();
-    console.log(tempArr)
-    console.log(excelFile)
-    for(let i = 0;i<excelFile.length;i++){
+    console.log(excelFile);
+    for (let i = 0; i < excelFile.length; i++) {
       if (excelFile[i] !== null) {
         const workbook = XLSX.read(excelFile[i], { type: "buffer" });
         const worksheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[worksheetName];
         const data = XLSX.utils.sheet_to_json(worksheet);
         data.forEach((element) => dataFixer(element));
+        console.log(fixedData);
         dataDeleeter(fixedData);
+        assignmentNameCounter(fixedData);
         setExcelData(fixedData);
         setCount(fixedData.length);
       }
     }
   };
+  //dataFixer getting the important data
   const dataFixer = (data) => {
     let temp = {};
-    let tempVal = "null";
     for (const [key, value] of Object.entries(data)) {
       const objectLength = Object.keys(temp).length;
       if (key == "First_Name") {
@@ -79,41 +84,70 @@ function App() {
         temp.Time_Spent = value;
       }
       if (objectLength >= 5) {
-        console.log("Temp being added: "+temp)
+        console.log("Temp being added: " + temp);
         fixedData.push(temp);
         temp = {};
         break;
       }
     }
   };
-  const dataDeleeter = (dataFixed) => {
-    for (let i = 0; i < dataFixed.length; i++) {
+  const dataDeleeter = (data) => {
+    for (let i = 0; i < data.length; i++) {
       if (
-        dataFixed[i].Total_Points_Earned ==
-          dataFixed[i].Total_Points_Possible &&
-        dataFixed[i].Time_Spent > 0
+        data[i].Total_Points_Earned == data[i].Total_Points_Possible &&
+        data[i].Time_Spent > 0
       ) {
         console.log(
           "adding idx: " +
             i +
             " pnts earned: " +
-            dataFixed[i].Total_Points_Earned +
+            data[i].Total_Points_Earned +
             " pos pnts: " +
-            dataFixed[i].Total_Points_Possible+ " tim spent: " + dataFixed[i].Time_Spent
+            data[i].Total_Points_Possible +
+            " tim spent: " +
+            data[i].Time_Spent
         );
       } else {
         console.log(
           "getting rid of idx: " +
-          i +
-          " pnts earned: " +
-          dataFixed[i].Total_Points_Earned +
-          " pos pnts: " +
-          dataFixed[i].Total_Points_Possible+ " tim spent: " + dataFixed[i].Time_Spent
+            i +
+            " pnts earned: " +
+            data[i].Total_Points_Earned +
+            " pos pnts: " +
+            data[i].Total_Points_Possible +
+            " tim spent: " +
+            data[i].Time_Spent
         );
-        dataFixed.splice(i, 1);
+        data.splice(i, 1);
         i--;
       }
     }
+  };
+  const assignmentNameCounter = (data) => {
+    console.log(data);
+    //pAN = pastAssignmentName
+    let pAN = data[0].Assignment_Name;
+    let count = 0;
+    let temp = {};
+    const tempArr = []
+    //element is the obj ref
+    //index is the index of the array
+    //array is the whole array
+    data.forEach((element, index, array) => {
+      //cAN = currentAssignmentName
+      let cAN = element.Assignment_Name;
+      if (pAN != cAN) {
+        pAN = cAN;
+        count = 1;
+      } else {
+        count++;
+        temp[pAN] = count;
+        tempArr.push(temp)
+        temp = {}
+      }
+    });
+    console.log(tempArr);
+    setAssignmentCounter(tempArr);
   };
   return (
     <div className="wrapper">
@@ -137,7 +171,7 @@ function App() {
         )}
       </form>
       <div>
-        number of interns completed: {count}
+        {/*assignmentCounter.map*/}
       </div>
       {/* view data */}
       <div className="viewer">
@@ -147,7 +181,9 @@ function App() {
               <thead>
                 <tr>
                   {Object.keys(excelData[0]).map((key) => (
-                    <th key={key} className="tblLabels">{key}</th>
+                    <th key={key} className="tblLabels">
+                      {key}
+                    </th>
                   ))}
                 </tr>
               </thead>
@@ -155,7 +191,9 @@ function App() {
                 {excelData.map((individualExcelData, index) => (
                   <tr key={index}>
                     {Object.keys(individualExcelData).map((key) => (
-                      <td key={key} className="tblData">{individualExcelData[key]}</td>
+                      <td key={key} className="tblData">
+                        {individualExcelData[key]}
+                      </td>
                     ))}
                   </tr>
                 ))}
